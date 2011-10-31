d3ab2afff9d5c17c8b847d70843c4e43151ba371
Dusan Miloradovic <DMiloradovic@gasco.ae>
2011-10-31 09:23:57 +0400

    some changes

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
 (defproject clgam "1.0.0-SNAPSHOT"
   :description "gmeng"
-:dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.clojure/clojure-contrib "1.2.0"]
-		 [ring/ring-core "0.2.0"]
+		 [ring "1.0.0-RC1"]
 		 [enlive "1.0.0"]
-		 [ring/ring-jetty-adapter "0.2.0"]
 		 [congomongo "0.1.7"]
 		 [net.cgrand/moustache "1.1.0"]
 		 ]
   :dev-dependencies [[swank-clojure "1.2.1"]
-		     [ring/ring-devel "0.2.0"]
 		     ]
   )
--- src/clgam/web.clj
+++ src/clgam/web.clj
@@ -3,6 +3,7 @@
   (:use net.cgrand.enlive-html)
   (:use [net.cgrand.moustache :only [app]])
   (:use ring.middleware.file)
+  (:use ring.middleware.content-type)
   (:use  [clojure.contrib.str-utils :only [str-join]])
   (:use somnium.congomongo)
   )
@@ -16,30 +17,32 @@
 (set-connection! mongocon)
 
 (defn get-mongo-file[path]
-  (stream-from :testaj
-               (fetch-one-file :testaj :where
+  (stream-from :fs
+               (fetch-one-file :fs :where
                                {:filename (construct-url path)}
                                )))
 
 (def ruter (app
-(wrap-file "src/webstatic")
-["nosa"]
-(fn[req] 
-  {:status 200 :headers {"Content-Type" "text/html"}
-   :body    "Testic"})
-["mosa"]
-(fn[req] 
-  {:status 200 :headers {"Content-Type" "text/html"}
-   :body    "Testic?"})
-["mongoize" & putanja]
-(fn[req] 
-  {:status 200 :headers {"Content-Type" "text/html"}
-   :body (get-mongo-file putanja)})
-[&]
-(fn[req] 
-  {:status 400 :headers {"Content-Type" "text/html"}
-   :body    "Not found?"})
- ))
+	    (wrap-content-type)
+	    (wrap-file "src/webstatic")
+	    ["nosa"]
+	    (fn[req] 
+	      {:status 200 
+	       :body    "Testic"})
+	    ["mosa"]
+	    (fn[req] 
+	      {:status 200 
+	       :body    "Testic?"})
+	    ["mongoize" & putanja]
+	    (fn[req]
+	      (if-let [f (get-mongo-file putanja)]
+		({:status 200 :body f})
+		({:status 400})))
+	    [&]
+	    (fn[req] 
+	      {:status 400 
+	       :body    "Not found?"})
+	    ))
 
 (defn boot []
   (run-jetty #'ruter {:port 7079}))
