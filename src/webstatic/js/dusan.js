bd6ec1937675327c488ab1fff69199e1415f4b99
kristina <kristina@kristina.(none)>
2011-11-03 20:57:01 +0400

    sp js

---
+++ src/webstatic/js/dusan.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+	$('#txtValue').keyup(function(){
+                sendValue($(this).val());  
+               
+            });
+	$('#brdimg').click(function(e){
+		sendCoords(e,$(this));
+	    });
+	$.board = {
+	    xsize:$('#brdimg').width(),
+	    ysize:$('#brdimg').height()
+	};
+           
+    });
+function sendValue(str){
+    $.post("ajax/test/"+str, {name:"Dusan", surname:"Miloradovic"},
+	   function(data){
+	       $('#display').html(data);
+	   },"html");
+           
+}
+
+	
+function sendCoords(e,t){
+    var x=e.pageX - t.offset().left;
+    var y=e.pageY - t.offset().top;
+    $.post("ajax/test",{xcoord:x/$.board.xsize, ycoord:y/$.board.ysize});
+}
