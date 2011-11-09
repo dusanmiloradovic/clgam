db55f63ee111bdbde4c8a1d799139b8f7f35ff3b
Dusan Miloradovic <DMiloradovic@gasco.ae>
2011-11-09 10:04:17 +0400

    Positioning is correct now

--- src/webstatic/js/dusan.js
+++ src/webstatic/js/dusan.js
@@ -24,19 +24,20 @@ function sendValue(str){
 function sendCoords(e,t){
     var x=e.pageX - t.offset().left;
     var y=e.pageY - t.offset().top;
-    alert (t.offset().left);
     $.post("tictactoe",{xcoord:x/$.board.xsize, ycoord:y/$.board.ysize},
 	   function (data){
 	       var xField=jQuery.parseJSON(data).xfield;
        	       var yField=jQuery.parseJSON(data).yfield;
 	       var xDraw=(xField+0.5)* ($.board.xsize/3);
 	       var yDraw=(yField+0.5)* ($.board.ysize/3);
-	       xDraw=0;
-	       yDraw=0;
 	       $("#polje_"+xField+"_"+yField).remove();
-
-	       $("#igra").append($("<div id='polje_"+xField+"_"+yField+"'> <img id='figimg' src='img/figura.jpg' /> </div>").css(
-																 {'position':'absolute','z-index':'1', 'left':(xDraw+'px'), 'top':(yDraw+'px')}
+	       obja=$("<div id='polje_"+xField+"_"+yField+"'> <img id='figimg' src='img/figura.jpg' /> </div>");
+	       $("#igra").append(obja.css(
+		   {'text-align':'center','position':'absolute','z-index':'1', 'left':(xDraw+'px'), 'top':(yDraw+'px')}
 					 ));
+       	       var dOffsetx=obja.width()/2;
+       	       var dOffsety=obja.height()/2;
+	       obja.css({'left':'-='+dOffsetx,'top':'-='+dOffsety});
+
 	   });
 }
