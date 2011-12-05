65a1227519a5cce533c163efe85d5be3018fc50b
Dusan Miloradovic <DMiloradovic@gasco.ae>
2011-12-05 16:05:13 +0400

    jos malo promena

--- src/webstatic/js/dusan.js
+++ src/webstatic/js/dusan.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
-	$('#brdimg').click(function(e){
-		sendCoords(e,$(this));
-	    });
+    $('#brdimg').click(function(e){
+	sendCoords(e,$(this));
+    });
     $('#sg123').click(function(e){
 	$.post("startgame",{game_name:"tictactoe"});
     });
+    $("igra").hide();
 
-
-	waitForMsg();
-	waitForInvitations();
-    });
+    waitForMsg();
+    waitForInvitations();
+});
 
 $(window).load(function (e){
-    	$.board = {
-	    xsize:$('#brdimg').width(),
-	    ysize:$('#brdimg').height()
-	};
-    });
+    $.board = {
+	xsize:$('#brdimg').width(),
+	ysize:$('#brdimg').height()
+    };
+});
 
-	
+
 function sendCoords(e,t){
     var x=e.pageX - t.offset().left;
     var y=e.pageY - t.offset().top;
@@ -33,8 +33,8 @@ function displayField(data){
     $("#polje_"+xField+"_"+yField).remove();
     obja=$("<div id='polje_"+xField+"_"+yField+"'> <img id='figimg' src='img/figura.jpg' /> </div>");
     $("#igra").append(obja.css(
-			       {'text-align':'center','position':'absolute','z-index':'1', 'left':(xDraw+'px'), 'top':(yDraw+'px')}
-			       ));
+	{'text-align':'center','position':'absolute','z-index':'1', 'left':(xDraw+'px'), 'top':(yDraw+'px')}
+    ));
     var dOffsetx=obja.width()/2;
     var dOffsety=obja.height()/2;
     obja.css({'left':'-='+dOffsetx,'top':'-='+dOffsety});
@@ -43,54 +43,65 @@ function displayField(data){
 
 
 function waitForMsg(){
-     $.ajax({
-            type: "GET",
-		url: "fieldsout",
+    $.ajax({
+        type: "GET",
+	url: "fieldsout",
 
-		async: true, /* If set to non-async, browser shows page as "Loading.."*/
-		cache: false,
-		timeout:50000, /* Timeout in ms */
+	async: true, /* If set to non-async, browser shows page as "Loading.."*/
+	cache: false,
+	timeout:50000, /* Timeout in ms */
 
-		success: function(data){ /* called when request to barge.php completes */
-		    
-		displayField(data);
-		waitForMsg();
-            },
-		error: function(XMLHttpRequest, textStatus, errorThrown){
-		waitForMsg();
-	    }
-        });
+	success: function(data){ /* called when request to barge.php completes */
+	    
+	    displayField(data);
+	    waitForMsg();
+        },
+	error: function(XMLHttpRequest, textStatus, errorThrown){
+	    waitForMsg();
+	}
+    });
 }
 
 function waitForInvitations(){
     $.ajax({
-            type: "GET",
-		url: "pending",
+        type: "GET",
+	url: "pending",
 
-		async: true, /* If set to non-async, browser shows page as "Loading.."*/
-		cache: false,
-		timeout:50000, /* Timeout in ms */
+	async: true, /* If set to non-async, browser shows page as "Loading.."*/
+	cache: false,
+	timeout:50000, /* Timeout in ms */
 
-		success: function(data){ /* called when request to barge.php completes */
-		    
-		printInvitations(data);
-		waitForInvitations();
-            },
-		error: function(XMLHttpRequest, textStatus, errorThrown){
-		waitForInvitations();
-	    }
-        });
+	success: function(data){ /* called when request to barge.php completes */
+	    
+	    printInvitations(data);
+	    waitForInvitations();
+        },
+	error: function(XMLHttpRequest, textStatus, errorThrown){
+	    waitForInvitations();
+	}
+    });
 }
 
+
+function joinGame(sGameName,sGameUid){
+    $.post("joingame",{gamename:sGameName, game_uid:sGameUid},
+	   function(data){
+	       $("igra").show();
+	   });
+}
+
+
+
+}
 function printInvitations(data){
     var jData=$.parseJSON(data);
     var invitations="";
     for (var prop in jData){
 	var val=jData[prop];
-	invitations+="<a href='/joingame?gamename=tictactoe&game_uid="+prop+"'>"+val+"</a><br/>";
+	invitations+="<a href=# onclick='joinGame(\"tictactoe\",\""+prop+"\");'>"+val+"</a><br/>";
     }
     $("#opengames").html(invitations);
-   
+    
 }
 
 
